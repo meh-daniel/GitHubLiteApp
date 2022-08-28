d159dae2902def1e1e2c283cf4a3abf01f4cce4a
Danil Galkin <mehanikdanil@gmail.com>
2022-08-28 11:08:49 +0700

    Реализация репозитория

--- app/src/main/java/meh/daniel/com/githubliteapp/data/AppRepositoryImpl.kt
+++ app/src/main/java/meh/daniel/com/githubliteapp/data/AppRepositoryImpl.kt
@@ -4,7 +4,7 @@ import meh.daniel.com.githubliteapp.domain.AppRepository
 import meh.daniel.com.githubliteapp.domain.model.Repository
 
 class AppRepositoryImpl : AppRepository {
-    override suspend fun getRepositories(userName: String): List<Repository> {
+    override suspend fun getRepositoriesByName(userName: String): List<Repository> {
         TODO("Not yet implemented")
     }
 }
--- app/src/main/java/meh/daniel/com/githubliteapp/domain/AppRepository.kt
+++ app/src/main/java/meh/daniel/com/githubliteapp/domain/AppRepository.kt
@@ -5,5 +5,5 @@ import meh.daniel.com.githubliteapp.domain.model.RepositoryDetails
 import meh.daniel.com.githubliteapp.domain.model.UserInfo
 
 interface AppRepository {
-    suspend fun getRepositories(userName: String): List<Repository>
+    suspend fun getRepositoriesByName(userName: String): List<Repository>
 }
