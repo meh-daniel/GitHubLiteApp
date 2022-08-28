357a1c612dd501b6d622f52fa58e0362362eedf2
Danil Galkin <mehanikdanil@gmail.com>
2022-08-28 12:50:51 +0700

    Адаптер созданный для экрана со списком репозиториев

---
+++ app/src/main/java/meh/daniel/com/githubliteapp/presentation/repositorieslist/RepositoriesListAdapter.kt
@@ -0,0 +1,50 @@
+package meh.daniel.com.githubliteapp.presentation.repositorieslist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import meh.daniel.com.githubliteapp.R
+import meh.daniel.com.githubliteapp.databinding.ItemRepositoryBinding
+import meh.daniel.com.githubliteapp.domain.model.Repository
+
+class RepositoryAdapter : ListAdapter<Repository, RecyclerView.ViewHolder>(RepositoryDiffUtil()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = when(viewType){
+        R.layout.item_repository -> RepositoryViewHolder.from(parent)
+        else ->  throw Throwable("onCreateViewHolder exception - unknown view type by name: $viewType")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) = when(holder) {
+        is RepositoryViewHolder -> holder.bind(item = getItem(position))
+        else -> throw Throwable("onBindViewHolder exception - unknown holder of view by name ${holder.itemView.id}")
+    }
+}
+
+class RepositoryViewHolder(private val binding: ItemRepositoryBinding) : RecyclerView.ViewHolder(binding.root){
+    fun bind(item: Repository){
+        binding.nameRepositoryTxt.text = item.name
+        binding.programmingLanguageTxt.text = item.language
+        binding.repositoryDescription.text = item.description
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): RecyclerView.ViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = ItemRepositoryBinding.inflate(layoutInflater, parent, false)
+            return RepositoryViewHolder(binding)
+        }
+    }
+}
+
+class RepositoryDiffUtil: DiffUtil.ItemCallback<Repository>() {
+    override fun areItemsTheSame(
+        oldItem: Repository,
+        newItem: Repository
+    ): Boolean = oldItem == newItem
+
+    override fun areContentsTheSame(
+        oldItem: Repository,
+        newItem: Repository
+    ): Boolean = oldItem == newItem
+}
