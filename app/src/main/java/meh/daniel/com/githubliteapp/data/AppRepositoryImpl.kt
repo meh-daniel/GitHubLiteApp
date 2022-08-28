f5416ff76c22a25063a2673ca860a67221c02e9d
Danil Galkin <mehanikdanil@gmail.com>
2022-08-28 11:06:12 +0700

    Метод с получением репозиториев по пользователю

---
+++ app/src/main/java/meh/daniel/com/githubliteapp/data/AppRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package meh.daniel.com.githubliteapp.data
+
+import meh.daniel.com.githubliteapp.domain.AppRepository
+import meh.daniel.com.githubliteapp.domain.model.Repository
+
+class AppRepositoryImpl : AppRepository {
+    override suspend fun getRepositories(userName: String): List<Repository> {
+        TODO("Not yet implemented")
+    }
+}
