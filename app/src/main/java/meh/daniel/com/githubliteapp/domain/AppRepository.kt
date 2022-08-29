b17ef92a7a67a0761b2d5a96227fbcacd570d1d7
Danil Galkin <mehanikdanil@gmail.com>
2022-08-29 14:09:40 +0700

    Что сделано 1. Добавлены реализации интерфейсов ретрофита с сетевыми запросами которые соответствуют заданию 2. Сделаны репозитории в соответствии с ТЗ 3. Маперы к переводу из modelNW -> Domain 4. Ренейминг моделей

--- app/src/main/java/meh/daniel/com/githubliteapp/domain/AppRepository.kt
+++ app/src/main/java/meh/daniel/com/githubliteapp/domain/AppRepository.kt
@@ -5,6 +5,8 @@ import meh.daniel.com.githubliteapp.domain.model.RepositoryDetails
 import meh.daniel.com.githubliteapp.domain.model.UserInfo
 
 interface AppRepository {
-    suspend fun getRepositoriesByNameUser(userName: String): List<Repository>
-    suspend fun getRepositoriesByID(id: Int): RepositoryDetails
+    suspend fun getRepositories(userName: String): List<Repository>
+    suspend fun getRepository(id: Int): RepositoryDetails
+    suspend fun getRepositoryReadme(ownerName: String, repositoryName: String, branchName: String): String
+    suspend fun signIn(token: String): UserInfo
 }
