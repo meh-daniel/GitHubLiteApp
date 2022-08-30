d5254e8a871490d1500ed0bcca95c60caac4be82
Danil Galkin <mehanikdanil@gmail.com>
2022-08-30 13:35:11 +0700

    Что сделано 1. Data module- добавлен провайдер для репозитория с токеном 2. TokenRepositoryImpl- добавлена черновая реализация валидации данных под будущую реаллизацию паттерна: "цепочка обязанностей".

--- app/src/main/java/meh/daniel/com/githubliteapp/di/DataModule.kt
+++ app/src/main/java/meh/daniel/com/githubliteapp/di/DataModule.kt
@@ -7,17 +7,26 @@ import dagger.hilt.components.SingletonComponent
 import javax.inject.Named
 import javax.inject.Singleton
 import meh.daniel.com.githubliteapp.data.AppRepositoryImpl
+import meh.daniel.com.githubliteapp.data.TokenRepositoryImpl
 import meh.daniel.com.githubliteapp.data.nw.GitHubApi
 import meh.daniel.com.githubliteapp.domain.AppRepository
+import meh.daniel.com.githubliteapp.domain.TokenRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DataModule {
     @Provides
     @Singleton
-    fun provideGitHubRepository(
+    fun provideAppRepository(
         @Named("GitHubApi") gitHubApi: GitHubApi
     ) : AppRepository {
         return AppRepositoryImpl(gitHubApi)
+    }
+    @Provides
+    @Singleton
+    fun provideTokenRepository(
+        @Named("GitHubApi") gitHubApi: GitHubApi
+    ) : TokenRepository {
+        return TokenRepositoryImpl(gitHubApi)
     }
 }
