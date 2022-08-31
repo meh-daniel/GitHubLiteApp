461666b3b8e80e1c305d5901571513b7b088c276
Danil Galkin <mehanikdanil@gmail.com>
2022-08-31 15:51:36 +0700

    Убрал из репозитория возможность в параметр добавлять токен, так как токен должен браться прям в репозитория из sharedPreferences

--- app/src/main/java/meh/daniel/com/githubliteapp/data/repositories/AppRepositoryImpl.kt
+++ app/src/main/java/meh/daniel/com/githubliteapp/data/repositories/AppRepositoryImpl.kt
@@ -1,6 +1,7 @@
-package meh.daniel.com.githubliteapp.data
+package meh.daniel.com.githubliteapp.data.repositories
 
 import meh.daniel.com.githubliteapp.data.nw.GitHubApi
+import meh.daniel.com.githubliteapp.data.toDomain
 import meh.daniel.com.githubliteapp.domain.AppRepository
 import meh.daniel.com.githubliteapp.domain.model.repository.Repository
 import meh.daniel.com.githubliteapp.domain.model.repository.RepositoryDetails
@@ -8,9 +9,9 @@ import meh.daniel.com.githubliteapp.domain.model.repository.RepositoryDetails
 class AppRepositoryImpl(
     private val gitHubApi: GitHubApi
 ) : AppRepository {
-    override suspend fun getRepositories(userName: String): List<Repository> {
+    override suspend fun getRepositories(): List<Repository> {
         return try {
-             gitHubApi.getRepositories(userName).toDomain()
+             gitHubApi.getRepositories("Token jfoisjgfouehrg").toDomain()
         } catch (e : Throwable) {
              throw Throwable("myError ${e.message.orEmpty()}")
         }
--- app/src/main/java/meh/daniel/com/githubliteapp/domain/AppRepository.kt
+++ app/src/main/java/meh/daniel/com/githubliteapp/domain/AppRepository.kt
@@ -4,7 +4,7 @@ import meh.daniel.com.githubliteapp.domain.model.repository.Repository
 import meh.daniel.com.githubliteapp.domain.model.repository.RepositoryDetails
 
 interface AppRepository {
-    suspend fun getRepositories(userName: String): List<Repository>
+    suspend fun getRepositories(): List<Repository>
     suspend fun getRepository(id: Int): RepositoryDetails
     suspend fun getRepositoryReadme(ownerName: String, repositoryName: String, branchName: String): String
 }
