cc2d90d2c218dd2cc5288218338ed259031708b0
Danil Galkin <mehanikdanil@gmail.com>
2022-08-31 16:51:23 +0700

    Эксперементально, под будущий рефакторинг кода оставлю

--- app/src/main/java/meh/daniel/com/githubliteapp/presentation/NavigationExtensions.kt
+++ app/src/main/java/meh/daniel/com/githubliteapp/presentation/NavigationExtensions.kt
@@ -1,18 +1,18 @@
-package meh.daniel.com.githubliteapp.presentation
-
-import androidx.annotation.IdRes
-import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.NavDirections
-import androidx.navigation.findNavController
-import meh.daniel.com.githubliteapp.R
-
-fun Fragment.activityNavController() = requireActivity().findNavController(R.id.main_nav)
-
-fun NavController.navigateSafely(@IdRes actionId: Int) {
-    currentDestination?.getAction(actionId)?.let { navigate(actionId) }
-}
-
-fun NavController.navigateSafely(directions: NavDirections) {
-    currentDestination?.getAction(directions.actionId)?.let { navigate(directions) }
-}
+//package meh.daniel.com.githubliteapp.presentation
+//
+//import androidx.annotation.IdRes
+//import androidx.fragment.app.Fragment
+//import androidx.navigation.NavController
+//import androidx.navigation.NavDirections
+//import androidx.navigation.findNavController
+//import meh.daniel.com.githubliteapp.R
+//
+//fun Fragment.activityNavController() = requireActivity().findNavController(R.id.main_nav)
+//
+//fun NavController.navigateSafely(@IdRes actionId: Int) {
+//    currentDestination?.getAction(actionId)?.let { navigate(actionId) }
+//}
+//
+//fun NavController.navigateSafely(directions: NavDirections) {
+//    currentDestination?.getAction(directions.actionId)?.let { navigate(directions) }
+//}
---
+++ app/src/main/java/meh/daniel/com/githubliteapp/presentation/UIState.kt
@@ -0,0 +1,8 @@
+//package meh.daniel.com.githubliteapp.presentation
+//
+//sealed class UIState<T> {
+//    class Idle<T> : UIState<T>()
+//    class Loading<T> : UIState<T>()
+//    class Error<T>(val error: String) : UIState<T>()
+//    class Success<T>(val data: T) : UIState<T>()
+//}
